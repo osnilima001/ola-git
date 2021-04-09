0f8d81453b1681f8a2b46e9b58620e372d766be8
Osni Lima <osnilima@live.com>
2021-04-09 00:22:03 +0000

    Script agnóstivo

--- main.sh
+++ main.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y --simulate mariadb-server
+source /etc/os-release
+
+function _debian {
+	dpkg -l | grep mariadb-server > /dev/null
+	test $? -eq 0 && return
+	apt-get update && apt-get install -y --simulate mariadb-server
+}
+
+function _centos {
+	rpm -qa | grep mariadb-server > /dev/null
+	teste $? -eq 0 && return
+	dnf install -y mariadb-server
+}
+
+_$ID
+
